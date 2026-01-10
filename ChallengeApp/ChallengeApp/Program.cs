57d14f83c1adae71680170e034678ce2b868c100
Piotr_Grzela <ipiotrgrzela@hotmail.com>
2026-01-10 13:46:28 +0100

    Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Piotr");
+﻿var name = "Ewa";
+var sex = "female";
+var age = 33;
+
+if (sex  == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat .");
+}
+else if(name == "Ewa"&& age == 30) 
+
+{
+    Console.WriteLine("Ewa lat 30 .");
+}
+else if(sex != "female" && age <18)
+    {
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+// Wszystkie warunki 
+else if (sex == "female"|| age <30 || name == "Ewa" || age == 30)
+{
+    
+}
